923526aca4ba4ff2b3ed8b507f3d62d1c21487db
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-06-03 17:54:49 -0300

    Fondo, imagen y textos del login

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -5,9 +5,79 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: 50,
+        child: _textDontHaveAccount(),
+      ),
+      body: Stack(//Para pocicionarl elementos uno encima del otro
+        children: [
+          _backgroundCover(context),
+          Column(//Para posicionar elementos uno abajo del otro
+            children: [
+              _imageCover(),
+              _textAppName(),
+            ],
+          )
+        ],
+      )
+    );
+  }
+
+  // Fondo de la aplicacion
+  Widget _backgroundCover(BuildContext context){
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.4,
+      color: Colors.green,
+    );
+  }
+  // Titulo de la aplicacion
+  Widget _textAppName(){
+    return Text(
+      'Delivery App',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Colors.black
+      ),
+    );
+  }
+  // Textos de registro y crear cuenta
+  Widget _textDontHaveAccount(){
+    return Row(//Ubicar elementos uno al lado del otro (Horizontal)
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '¿No tenes cuenta?',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 17
+          ),
+        ),
+        SizedBox(width: 7),
+        Text(
+            'Registrate aca',
+          style: TextStyle(
+              color: Colors.green,
+              fontWeight: FontWeight.bold,
+              fontSize: 19
+          ),
+        )
+      ],
+    );
+  }
+  // Imagen del login
+  Widget _imageCover(){
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 20, bottom: 15),
+        alignment: Alignment.topCenter,
+        child: Image.asset(
+          'assets/img/delivery.png',
+          width: 130,
+          height: 130,
+        ),
       ),
     );
   }
