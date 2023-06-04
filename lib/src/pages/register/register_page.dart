966f0a5e8d54520c445480921f19beb6d0b56820
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-06-04 00:41:47 -0300

    Creacion de pantalla de registro

--- lib/src/pages/register/register_page.dart
+++ lib/src/pages/register/register_page.dart
@@ -1,13 +1,216 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('REGISTER PAGE'),
+    return Scaffold(
+      body: Stack(//Para posicionar elementos uno encima del otro
+        children: [
+          _backgroundCover(context),
+          _boxForm(context),
+          _imageUser(),
+          _buttonBack(),
+        ],
+      ),
+    );
+  }
+
+  // Boton para retroceder
+  Widget _buttonBack() {
+    return SafeArea(
+        child: Container(
+          margin: EdgeInsets.only(left: 20),
+          child: IconButton(
+            onPressed:() => Get.back(),
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+              size: 30,
+            )
+          ),
+        )
+    );
+  }
+  // Fondo de la aplicacion
+  Widget _backgroundCover(BuildContext context){
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.35,
+      color: Colors.green,
+    );
+  }
+
+  // Box de Registro
+  Widget _boxForm(BuildContext context){
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.65,
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.30, left: 50, right: 50),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black54,
+                blurRadius: 15,
+                offset: Offset(0, 0.75)
+            )
+          ]
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            _textYourInfo(),
+            _textFieldEmail(),
+            _textFieldName(),
+            _textFieldLastName(),
+            _textFieldLastPhone(),
+            _textFieldPassword(),
+            _textFieldConfirmPassword(),
+            _buttonRegister()
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Input de Email
+  Widget _textFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            hintText: 'Correo electronico',
+            prefixIcon: Icon(Icons.email)
+        ),
+      ),
+    );
+  }
+
+  // Input de Nombre
+  Widget _textFieldName() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(
+            hintText: 'Nombre',
+            prefixIcon: Icon(Icons.person)
+        ),
+      ),
+    );
+  }
+
+  // Input de Apellido
+  Widget _textFieldLastName() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(
+            hintText: 'Apellido',
+            prefixIcon: Icon(Icons.person_outline)
+        ),
+      ),
+    );
+  }
+
+  // Input de Celular
+  Widget _textFieldLastPhone() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        keyboardType: TextInputType.phone,
+        decoration: InputDecoration(
+            hintText: 'Celular',
+            prefixIcon: Icon(Icons.phone)
+        ),
+      ),
+    );
+  }
+
+  // Input de contraseña
+  Widget _textFieldPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        keyboardType: TextInputType.text,
+        obscureText: true,
+        decoration: InputDecoration(
+            hintText: 'Contraseña',
+            prefixIcon: Icon(Icons.lock)
+        ),
+      ),
+    );
+  }
+
+  // Input de confirmar contraseña
+  Widget _textFieldConfirmPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        keyboardType: TextInputType.text,
+        obscureText: true,
+        decoration: InputDecoration(
+            hintText: 'Confirmar contraseña',
+            prefixIcon: Icon(Icons.lock_outline)
+        ),
+      ),
+    );
+  }
+
+  // Imagen del registro de usuario
+  Widget _imageUser() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top:25),
+        alignment: Alignment.topCenter,
+        child: GestureDetector(
+          onTap: () {},
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/img/user_profile.png'),
+            radius: 60,
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Texto de Ingresa los datos
+  Widget _textYourInfo(){
+    return Container(
+      margin: EdgeInsets.only(top: 40, bottom: 30),
+      child: Text(
+        'INGRESE LOS DATOS',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+
+        ),
+      ),
+    );
+  }
+
+  // Boton
+  Widget _buttonRegister() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 40 ),
+      child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 15)
+          ),
+          child: Text(
+            'REGISTRARSE',
+            style: TextStyle(
+                color: Colors.black
+            ),
+          )
       ),
     );
   }
