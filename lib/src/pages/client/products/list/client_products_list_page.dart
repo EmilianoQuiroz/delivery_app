851356627227c69c9d0497e5424aa64c23905f98
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-06-12 23:32:58 -0300

    Buttom Navigation Bar

--- lib/src/pages/client/products/list/client_products_list_page.dart
+++ lib/src/pages/client/products/list/client_products_list_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:delivery_app/src/pages/client/products/list/client_products_list_controller.dart';
+import 'package:delivery_app/src/pages/delivery/orders/list/delivery_orders_list_page.dart';
+import 'package:delivery_app/src/pages/register/register_page.dart';
+import 'package:delivery_app/src/pages/restaurant/orders/list/restaurant_orders_list_page.dart';
+import 'package:delivery_app/src/utils/custom_animated_bottom_bar.dart';
 
 class ClientProductsListPage extends StatelessWidget {
 
@@ -8,24 +12,50 @@ class ClientProductsListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        bottomNavigationBar: _bottomBar(),
+        body: Obx(() => IndexedStack(
+          index: con.indexTab.value,
           children: [
-            Text('Client Products List'),
-            ElevatedButton(
-              onPressed: () => con.signOut(),
-              child: Text(
-                'Cerrar sesion',
-                style: TextStyle(
-                    color: Colors.black
-                ),
-              ),
-            )
+            RestaurantOrdersListPage(),
+            DeliveryOrdersListPage(),
+            RegisterPage()
           ],
-        ),
-      ),
+        ))
     );
   }
+
+  Widget _bottomBar() {
+    return Obx(() => CustomAnimatedBottomBar(
+      containerHeight: 70,
+      backgroundColor: Colors.amber,
+      showElevation: true,
+      itemCornerRadius: 24,
+      curve: Curves.easeIn,
+      selectedIndex: con.indexTab.value,
+      onItemSelected: (index) => con.changeTab(index),
+      items: [
+        BottomNavyBarItem(
+            icon: Icon(Icons.apps),
+            title: Text('Home'),
+            activeColor: Colors.white,
+            inactiveColor: Colors.black
+        ),
+        BottomNavyBarItem(
+            icon: Icon(Icons.list),
+            title: Text('Mis pedidos'),
+            activeColor: Colors.white,
+            inactiveColor: Colors.black
+        ),
+        BottomNavyBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Perfil'),
+            activeColor: Colors.white,
+            inactiveColor: Colors.black
+        ),
+      ],
+    ));
+  }
+
 }
