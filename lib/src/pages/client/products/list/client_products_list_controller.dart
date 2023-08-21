bd1c377a7f40275033ade24105876cd5a0dcdb0b
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-08-21 00:07:28 -0300

    Mostrar detalle de prod

--- lib/src/pages/client/products/list/client_products_list_controller.dart
+++ lib/src/pages/client/products/list/client_products_list_controller.dart
@@ -1,18 +1,57 @@
-import 'package:delivery_app/src/models/category.dart';
-import 'package:delivery_app/src/providers/categories_provider.dart';
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:delivery_app/src/providers/products_provider.dart';
-import 'package:delivery_app/src/models/product.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:delivery_app/src/models/category.dart';
+import 'package:delivery_app/src/models/product.dart';
+import 'package:delivery_app/src/pages/client/products/detail/client_products_detail_controller.dart';
+import 'package:delivery_app/src/pages/client/products/detail/client_products_detail_page.dart';
+import 'package:delivery_app/src/providers/categories_provider.dart';
+import 'package:delivery_app/src/providers/products_provider.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class ClientProductsListController extends GetxController {
+
   CategoriesProvider categoriesProvider = CategoriesProvider();
   ProductsProvider productsProvider = ProductsProvider();
 
-  List<Category> categories = <Category>[].obs;
+  List<Product> selectedProducts = [];
 
-  ClientProductsListController(){
+
+  List<Category> categories = <Category>[].obs;
+  var items = 0.obs;
+
+  var productName = ''.obs;
+  Timer? searchOnStoppedTyping;
+
+  ClientProductsListController() {
     getCategories();
+    if (GetStorage().read('shopping_bag') != null) {
+      if (GetStorage().read('shopping_bag') is List<Product>) {
+        selectedProducts = GetStorage().read('shopping_bag');
+      }
+      else {
+        selectedProducts = Product.fromJsonList(GetStorage().read('shopping_bag'));
+      }
+
+      selectedProducts.forEach((p) {
+        items.value = items.value + (p.quantity!);
+      });
+
+    }
+  }
+
+  void onChangeText(String text) {
+    const duration = Duration(milliseconds: 800);
+    if (searchOnStoppedTyping != null) {
+      searchOnStoppedTyping?.cancel();
+    }
+
+    searchOnStoppedTyping = Timer(duration, () {
+      productName.value = text;
+      print('TEXTO COMPLETO: ${text}');
+    });
   }
 
   void getCategories() async {
@@ -22,6 +61,7 @@ class ClientProductsListController extends GetxController {
   }
 
   Future<List<Product>> getProducts(String idCategory, String productName) async {
+
     if (productName.isEmpty) {
       return await productsProvider.findByCategory(idCategory);
     }
@@ -30,4 +70,17 @@ class ClientProductsListController extends GetxController {
     }
 
   }
+
+  void goToOrderCreate() {
+    Get.toNamed('/client/orders/create');
+  }
+
+  void openBottomSheet(BuildContext context, Product product) async{
+
+    showMaterialModalBottomSheet(
+      context: context,
+      builder: (context) => ClientProductsDetailPage(product: product),
+    );
+  }
+
 }
